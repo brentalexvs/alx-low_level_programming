0c9d8432bae1b8368338d344e261040c5180b0ae
brentalexvs <brentalexvs@gmail.com>
2023-10-09 16:03:03 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdi.h>
+
+/**
+ * main - Write function to produce lower and uppercase alphabet
+ *
+ * Return - always 0
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+		putchar(a);
+	for (a = 'A' ; a <= 'Z' ; a++)
+		putchar(a);
+	putchar('\n')
+	return (0);
+}
