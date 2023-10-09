5de0ee7a088752edf08a999ea626abfe4637b623
brentalexvs <brentalexvs@gmail.com>
2023-10-09 16:50:17 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print single digits from base 10 to 0 with putchar
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+	for (n = 0 ; n < 10 ; n++)
+	       	putchar(n + '0');
+	putchar('\n');
+	return (0);
+}
