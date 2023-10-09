a18adcf6f140c0393f7e2976bde598cac58bd189
brentalexvs <brentalexvs@gmail.com>
2023-10-09 15:51:19 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * This function is to display the alphabet
+ *
+ * Return - always 0
+ */
+
+int main(void)
+{
+	for (a = 'a' ; a <= 'z' ; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
