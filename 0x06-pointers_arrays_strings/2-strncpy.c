c09884bac79219d33b92db966e95f71154f1d48d
brentalexvs <brentalexvs@gmail.com>
2023-10-31 13:03:36 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - a function that copies a string
+ * @dest: funtion one
+ * @src: function two
+ * @n: function three
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n && src[i] != '\0'; i++)
+		while (i < n)
+		{
+			dest[i] = '\0';
+			i++;
+		}
+	return (dest);
+}
