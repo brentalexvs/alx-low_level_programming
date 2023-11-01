8668c80c702c8e59e62358c6b30840b4316bdcbe
brentalexvs <brentalexvs@gmail.com>
2023-11-01 03:43:42 -0700

    0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print the programm name
+ * @argc: the arugment counter
+ * @argv: the argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
