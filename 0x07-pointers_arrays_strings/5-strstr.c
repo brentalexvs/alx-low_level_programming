5dc991fe4e93d585db2c478a85be4c264cbe6ece
brentalexvs <brentalexvs@gmail.com>
2023-11-01 00:01:12 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strstr - Functtion that locates a substring
+ * @needle: the substring
+ * @haystack: the string
+ * Return: haystack or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (*haystack = 0 ; *haystack != '\0' ; haystack++)
+	{
+		char *a = haystack;
+		char *b = needle;
+
+		while (*a == *b && *b != '\0')
+		{
+			a++;
+			b++;
+		}
+		if (*b == '\0')
+		return (haystack);
+	}
+	return (NULL);
+}
