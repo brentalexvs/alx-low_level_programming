16f2c12b708e88687a47f88c9b22481576e51389
brentalexvs <brentalexvs@gmail.com>
2023-11-06 12:39:55 -0800

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,12 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x)((x) < (0) ? -(x):(x))
+/**
+ * file - 3-function_like_macro
+ * @x: input value
+ * Return: x
+ */
+
+#define ABS(x)((x) < (0) ? -(1) * (x) : (x))
 
 #endif
